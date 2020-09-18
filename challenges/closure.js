d28612c5a7eb5ab5c01463c27dbf9fb6f9eddb54
Trey Roberts <treywroberts@gmail.comgit config --global user.name TreywRobertsrungit.addgit commit -m added>
2020-09-18 14:06:46 -0400

    hello

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//A function can reach outside of itself to grab a a varible but you can't reach into it.
 
 /* Task 2: Counter */
 
+function summation(num){
+  let score = 0;
+  return function add(){
+    score ++; 
+    return score = score + score;
+  }
+}
+console.log(summation(4))
+console.log(add())
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
